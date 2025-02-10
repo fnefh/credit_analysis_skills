35156d445ebc0b482a296dada6d6d707b26138f5
fnefh <131272511+fnefh@users.noreply.github.com>
2025-02-10 14:05:00 +0100

    final

--- transform.py
+++
@@ -1,8 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-source = "analysis.pdf"  # document per local path or URL
-converter = DocumentConverter()
-result = converter.convert(source)
-test=result.document.export_to_markdown()
-with open("analysis.md", "w") as f:
-    f.write(test)
